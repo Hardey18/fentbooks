a0b86d32b734245c655b59451bd80a4c97dabb63
Nurudeen Adewale <35535871+Hardey18@users.noreply.github.com>
2023-03-18 14:20:13 +0100

    Updated header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,26 @@
 import React from 'react'
+import { IoMdSettings, IoIosNotifications } from "react-icons/io"
 import "../styles/_header.scss"
 import logo from "/logo.png"
 
 function Header() {
   return (
     <div className="header-container">
-      <img className="logo" src={logo} alt="Logo" />
-      <div className="">Header</div>
+      <div className="top">
+        <img className="logo" src={logo} alt="Logo" />
+        <div className="icons">
+          <div className='first'>
+            <IoMdSettings size={25} />
+          </div>
+          <div className='second'>
+            <IoIosNotifications size={25} />
+          </div>
+        </div>
+      </div>
+      <div className="user">
+        <h4 className='welcome'>Welcome back,</h4>
+        <h2>Francis</h2>
+      </div>
     </div>
   )
 }
